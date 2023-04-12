f71c1279a90bad607de8af5e6b4be6bcce8dce30
unknown <louiecervantes@wvsu.edu.ph>
2023-04-12 16:44:08 +0800

    save changes

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -5,6 +5,23 @@ from textblob import TextBlob
 
 
 # Define the Streamlit app
+
+
+def getSubjectivity(text):
+    return TextBlob(text).sentiment.subjectivity
+  
+ #Create a function to get the polarity
+def getPolarity(text):
+    return TextBlob(text).sentiment.polarity
+
+def getAnalysis(score):
+    if score < 0:
+        return ‘Negative’
+elif score == 0:
+    return ‘Neutral’
+else:
+    return ‘Positive’
+    
 def app():
     st.title('Simple TextBlob Sentiment Demo')
     st.subheader('by Louie F. Cervantes, M.Eng.')
@@ -17,7 +34,10 @@ def app():
     
     # Add a button to update the data
     if st.button("Get sentiment"):
-        st.write(TextBlob("He is very good boy").sentiment)
+        polarity = getPolarity(user_input)
+        st.write('Polarity: ' + str(polarity))
+        st.write('Subjectivity: ' + getSubjectivity(user_input))
+        st.write('Sentiment: ' + getAnalysis(polarity))
 # run the app
 if __name__ == "__main__":
     app()
